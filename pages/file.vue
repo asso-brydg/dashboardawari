0eb8fe22b41edb9b7c8901422e9ae319e7a05609
charlesdzadu <dkccharlie@gmail.com>
2022-08-19 10:36:15 +0000

    Add download file working

--- pages/file.vue
+++ pages/file.vue
@@ -5,41 +5,26 @@
       <!-- A preview of the image. -->
       <img :src="blog.imageUrl" class="w-24 md:w-32 h-auto object-cover inline-block" alt="">
       <!-- Delete button for deleting the image. -->
-      <button
-        v-if="blog.imageUrl"
-        @click="deleteImage"
-        :disabled="isDeletingImage"
-        type="button"
-        class="bg-red-500 border-red-300 text-white"
-      >
+      <button v-if="blog.imageUrl" @click="deleteImage" :disabled="isDeletingImage" type="button"
+        class="bg-red-500 border-red-300 text-white">
         {{ isDeletingImage ? 'Deleting...' : 'Delete' }}
       </button>
     </div>
     <!-- Clicking this button triggers the "click" event of the file input. -->
-    <button
-      v-if="!blog.imageUrl"
-      @click="launchImageFile"
-      :disabled="isUploadingImage"
-      type="button"
-    >
+    <button v-if="!blog.imageUrl" @click="launchImageFile" :disabled="isUploadingImage" type="button">
       {{ isUploadingImage ? 'Uploading...' : 'Upload' }}
     </button>
     <!-- This is the real file input element. -->
-    <input
-      ref="imageFile"
-      @change.prevent="uploadImageFile($event.target.files)"
-      type="file"
-      accept="image/png, image/jpeg"
-      class="hidden"
-    >
+    <input ref="imageFile" @change.prevent="uploadImageFile($event.target.files)" type="file"
+      accept="image/png, image/jpeg" class="hidden">
   </div>
 </template>
 
 
 <script>
-import firebase from 'firebase/compat/app'
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 export default {
-  data () {
+  data() {
     return {
       blog: {},
       isUploadingImage: false,
@@ -47,11 +32,11 @@ export default {
     }
   },
   methods: {
-    launchImageFile () {
+    launchImageFile() {
       // Trigger the file input click event.
       this.$refs.imageFile.click()
     },
-    uploadImageFile (files) {
+    uploadImageFile(files) {
       if (!files.length) {
         return
       }
@@ -70,18 +55,50 @@ export default {
 
       // Create a reference to the destination where we're uploading
       // the file.
-      const storage = firebase.storage
-      const imageRef = storage.ref(`images/${files.name}`)
+      // const storage = this.$firebase.storage();
+      const storage = getStorage();
+      console.log("storage", storage);
 
-      const uploadTask = imageRef.put(file, metadata).then((snapshot) => {
-        // Once the image is uploaded, obtain the download URL, which
-        // is the publicly accessible URL of the image.
-        return snapshot.ref.getDownloadURL().then((url) => {
-          return url
-        })
-      }).catch((error) => {
-        console.error('Error uploading image', error)
-      })
+      const imageRef = ref(storage, `images/${files.name}`)
+      const uploadTask = uploadBytesResumable(imageRef, file);
+
+      uploadTask.on('state_changed',
+        (snapshot) => {
+          // Observe state change events such as progress, pause, and resume
+          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log('Upload is ' + progress + '% done');
+          switch (snapshot.state) {
+            case 'paused':
+              console.log('Upload is paused');
+              break;
+            case 'running':
+              console.log('Upload is running');
+              break;
+          }
+        },
+        (error) => {
+          // Handle unsuccessful uploads
+        },
+        () => {
+          // Handle successful uploads on complete
+          // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            
+            console.log('File available at', downloadURL);
+          });
+        }
+      );
+
+      // const uploadTask = imageRef.put(file, metadata).then((snapshot) => {
+      //   // Once the image is uploaded, obtain the download URL, which
+      //   // is the publicly accessible URL of the image.
+      //   return snapshot.ref.getDownloadURL().then((url) => {
+      //     return url
+      //   })
+      // }).catch((error) => {
+      //   console.error('Error uploading image', error)
+      // })
 
       // When the upload ends, set the value of the blog image URL
       // and signal that uploading is done.
@@ -90,7 +107,7 @@ export default {
         this.isUploadingImage = false
       })
     },
-    deleteImage () {
+    deleteImage() {
       firebase.storage().refFromURL(this.blog.imageUrl).delete()
         .then(() => {
           this.blog.imageUrl = ''

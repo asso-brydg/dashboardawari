e715db35cb77850105db5474675e912de70bbc6d
charlesdzadu <dkccharlie@gmail.com>
2022-08-19 10:42:01 +0000

    Add auth middleware working

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,6 +40,11 @@ export default {
     '@nuxtjs/pwa',
   ],
 
+
+  router: {
+    middleware: ['auth'],
+  },
+
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
